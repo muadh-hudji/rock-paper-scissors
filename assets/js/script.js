f7533470e5c9a7adcb3c54637c6af3dd214d4539
muadh hudji <muad.hudji@gmail.com>
2023-04-22 10:17:09 +0000

    Begin coding in playgame function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,41 @@
+/**
+ * Declare constants that needed in the script
+ */
 
-function checkChoice() {
+const choices = ["rock", "paper", "scissors", "lizard", "spock"];
+const buttons = document.getElementsByClassName('btn');
+
+/**
+ * Add event listener to all button choice of the game
+ */
+
+for(let button of buttons){
+    button.addEventListener("click", function(){
+        if(this.getAttribute("data-choice") < 5){
+            let userChoice = this.getAttribute("data-choice");
+            playGame(userChoice);
+        }
+    })
+}
+
+/**
+ * The function creates a choice for the computer
+ * and comparing it with the choice of the user.
+ * Disabling other choices for the user except
+ * playagain and reset.
+ */
+
+function playGame(userChoice) {
+    let compChoice = Math.floor(Math.random() * 5);
+    document.getElementsByClassName("rock").style.backgroundColor = "green";
+    // Disable other choices
+    for(let i = 0; i < 5; i++){
+        if(i === userChoice){
+
+            continue;
+        }
+        document.getElementsByClassName(choices[i]).disabled = true;
+    }
 
 }
 
@@ -13,4 +49,8 @@ function displayRoundResult() {
 
 function changeScore() {
     
+}
+
+function resetGame(){
+
 }
